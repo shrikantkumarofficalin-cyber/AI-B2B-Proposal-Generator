476d6bf323719448693ef9469d7d84d771d1fd33
shrikantkumarofficalin-cyber <shrikant.kumar.offical.in@gmail.com>
2026-03-07 15:42:33 +0530

    add requirements

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import json
+import os
 
 app = Flask(__name__)
 
@@ -66,5 +67,7 @@ Impact Summary:
 
     return render_template("index.html", text_result=text_result, json_result=json_result)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
